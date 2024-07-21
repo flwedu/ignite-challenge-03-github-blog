19316e5dd9de804ceb5de97c96531c3e54f5c05c
Eduardo Aquino <edutraquino@gmail.com>
2024-07-21 20:56:21 -0300

    Integrate react-query and react-router in main.tsx
    The code changes in main.tsx introduce both react-query and react-router. This aims to provide a basis for managing data fetching and routing in the application. The QueryClientProvider and RouterProvider have also been rendered at the root level.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import { RouterProvider } from "react-router-dom";
 import "./index.css";
+import { router } from "./router.tsx";
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<QueryClientProvider client={queryClient}>
+			<RouterProvider router={router} />
+		</QueryClientProvider>
+	</React.StrictMode>,
 );
